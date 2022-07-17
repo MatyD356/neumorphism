ac72b603ddbf4b3090e29c2d446c70098228bebd
matyd <matyd356@gmail.com>
2022-07-17 18:58:13 +0200

    add invoke limit to themeuUtton

--- src/components/themeButton/ThemeButton.tsx
+++ src/components/themeButton/ThemeButton.tsx
@@ -2,27 +2,32 @@ import useDarkTheme from "../../hooks/useDarkTheme"
 import sun from '../../assets/icons/sun.png'
 import moon from '../../assets/icons/moon.png'
 import { motion, Variants } from "framer-motion"
-import { useState } from "react"
+import { useCallback, useMemo, useState } from "react"
 import Button from "../button/Button"
+import thrrotle from 'lodash.throttle'
 
 export default function ThemeButton(){
     const [darkTheme, toggleTheme] = useDarkTheme()
     const [changeCounter, setChangeCounter] = useState(0)
+    const [disable, setDisable] = useState(false)
+    const rotationAngle = useMemo(() => 180,[])
+    const animationDuration = useMemo(() => 500,[])
     const variants: Variants = {
-        light: { rotate: 0+(changeCounter*180) },
-        dark: { rotate: 180*changeCounter }
+        light: { rotate: 0+(changeCounter*rotationAngle) },
+        dark: { rotate: rotationAngle*changeCounter }
     }
     const handleClick = () => {
-        console.log('xsd')
+      setDisable(true)
         setChangeCounter((prev) => prev+1)
         toggleTheme()
+        setTimeout(()=> setDisable(false), animationDuration)
     }
     return(
-        <Button onClick={handleClick} classes='w-28 h-14'>
+        <Button onClick={disable ? undefined : handleClick} classes='w-28 h-14'>
         <motion.div 
         animate={darkTheme ? 'dark' : 'light'}
         variants={variants}
-        transition={{duration: 0.5}}
+        transition={{duration: animationDuration/1000}}
         className={`absolute top-3 left-10 h-28 flex flex-col justify-between` }
         >
         <div>

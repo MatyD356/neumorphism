3c41e7007eae162e26ff726dd5ddec7be7cd2d88
mpyszny <matyd356@gmail.com>
2022-07-18 19:30:42 +0200

    imporve themeButtonLock

--- src/components/themeButton/ThemeButton.tsx
+++ src/components/themeButton/ThemeButton.tsx
@@ -2,32 +2,37 @@ import useDarkTheme from "../../hooks/useDarkTheme"
 import sun from '../../assets/icons/sun.png'
 import moon from '../../assets/icons/moon.png'
 import { motion, Variants } from "framer-motion"
-import { useCallback, useMemo, useState } from "react"
+import { useMemo, useState } from "react"
 import Button from "../button/Button"
-import thrrotle from 'lodash.throttle'
 
 export default function ThemeButton(){
+
     const [darkTheme, toggleTheme] = useDarkTheme()
     const [changeCounter, setChangeCounter] = useState(0)
     const [disable, setDisable] = useState(false)
-    const rotationAngle = useMemo(() => 180,[])
-    const animationDuration = useMemo(() => 500,[])
-    const variants: Variants = {
-        light: { rotate: 0+(changeCounter*rotationAngle) },
+
+    const rotationAngle = 180
+    const animationDuration = 0.5
+    const animationStartAngle =0
+
+    const variants: Variants = useMemo(()=>({
+        light: { rotate: animationStartAngle+(changeCounter*rotationAngle) },
         dark: { rotate: rotationAngle*changeCounter }
-    }
+    }),[changeCounter])
+
     const handleClick = () => {
-      setDisable(true)
+        setDisable(true)
         setChangeCounter((prev) => prev+1)
         toggleTheme()
-        setTimeout(()=> setDisable(false), animationDuration)
     }
+
     return(
         <Button onClick={disable ? undefined : handleClick} classes='w-28 h-14'>
         <motion.div 
         animate={darkTheme ? 'dark' : 'light'}
         variants={variants}
-        transition={{duration: animationDuration/1000}}
+        transition={{duration: animationDuration}}
+        onAnimationComplete={()=>setDisable(false)}
         className={`absolute top-3 left-10 h-28 flex flex-col justify-between` }
         >
         <div>
